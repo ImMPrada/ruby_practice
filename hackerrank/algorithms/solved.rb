350d5e923a945506782caa64d8aa1e8d09a86930
ImMPrada <im.mprada@gmail.com>
2022-05-06 13:03:09 -0500

    solve-2

--- hackerrank/algorithms/solved.rb
+++ hackerrank/algorithms/solved.rb
@@ -1,4 +1,5 @@
 #https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
 class SolveProposed
 
@@ -16,4 +17,18 @@ class SolveProposed
     long_sum += ar[size_to_cut/2] if size_odd
     long_sum
   end
+
+  def diagonalDifference(arr)
+    # Write your code here
+    i_limit = arr.size  
+    sum_diag_1 = 0
+    sum_diag_2 = 0
+    
+    (0...i_limit).each do |index|
+       sum_diag_1 += arr[index][index]
+       sum_diag_2 += arr[-index-1][index]
+    end
+    
+    (sum_diag_1-sum_diag_2).abs
+end
 end

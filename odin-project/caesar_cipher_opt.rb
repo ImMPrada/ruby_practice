94a152960308a1cd30bbd1dafcf468072ebc811f
ImMPrada <im.mprada@gmail.com>
2022-05-20 20:54:54 -0500

    wip

--- odin-project/caesar_cipher_opt.rb
+++ odin-project/caesar_cipher_opt.rb
@@ -4,6 +4,7 @@ require 'debug'
 def cipher(s, gap)
   alphabet = *('a'..'z')
   max_index = alphabet.size - 1
+  found = {}
 
   cipher = s.chars.reduce('')  do |s_cipher, letter|
     if letter == ' '
@@ -15,10 +16,15 @@ def cipher(s, gap)
 
       letter.downcase! if is_upper
 
-      index_cipher = alphabet.find_index(letter) + gap
-      index_cipher -= (max_index + 1) if index_cipher > max_index
-
-      letter_cipher = alphabet[index_cipher]
+      letter_cipher = found[letter.to_sym]
+      if !letter_cipher
+        index_cipher = alphabet.find_index(letter) + gap
+        index_cipher -= (max_index + 1) if index_cipher > max_index
+  
+        letter_cipher = alphabet[index_cipher]
+        found[letter.to_sym] = letter_cipher
+        puts(found)
+      end
       letter_cipher.upcase! if is_upper
 
       s_cipher + letter_cipher
@@ -28,7 +34,4 @@ def cipher(s, gap)
   cipher
 end
 
-puts(cipher('Ey hola', 5))
-puts(cipher('Ey hola', 6))
-puts(cipher('Ey hola', 0))
-puts(cipher('Ey hola', 2))
+puts(cipher('Ey holaholaholaholaholahola e', 5))

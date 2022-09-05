2c5a7f0a56932c1d90889da906a4dcdc03a77807
immprada <im.mprada@gmail.com>
2022-09-05 16:50:20 -0500

    mockup test

--- basic_ruby_projects/sub_strings/test.rb
+++ basic_ruby_projects/sub_strings/test.rb
@@ -2,4 +2,5 @@ require './basic_ruby_projects/sub_strings/main'
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit].freeze
 
-puts dictionary[0]
+puts substrings('below', dictionary)
+puts substrings("Howdy partner, sit down! How's it gogoinggo?", dictionary)

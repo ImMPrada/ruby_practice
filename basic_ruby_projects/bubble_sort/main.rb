db1ee3ebab48077fd9f9fb8fe999930db15b71b1
immprada <im.mprada@gmail.com>
2022-09-05 19:31:00 -0500

    initial setup

---
+++ basic_ruby_projects/bubble_sort/main.rb
@@ -0,0 +1,7 @@
+# https://www.theodinproject.com/lessons/ruby-bubble-sort
+
+puts 'Bubble Sort'
+
+def bubble_sort(numbers)
+  numbers
+end
---
+++ basic_ruby_projects/bubble_sort/test.rb
@@ -0,0 +1,4 @@
+require './basic_ruby_projects/bubble_sort/main'
+
+puts "[4, 3, 78, 2, 0, 1, 10, 5, 8, 20, 34, 45]"
+puts "#{bubble_sort([4, 3, 78, 2, 0, 1, 10, 5, 8, 20, 34, 45])}"
